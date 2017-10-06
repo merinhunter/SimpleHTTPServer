3c86e05c7f2f156ec83501ca25577d9576cf9ebc
Sergio Merino Hernández <sergiohdez94@gmail.com>
2017-10-06 22:28:52 +0200

    Cleaner code

--- server.py
+++ server.py
@@ -9,12 +9,6 @@ URL = "http://192.168.1.20" + ":" + str(PORT) + "/"
 
 class Handler(BaseHTTPRequestHandler):
 
-    def isDir(self, path):
-        return os.path.isdir(path)
-
-    def isFile(self, path):
-        return os.path.isfile(path)
-
     def getIndex(self, path):
         try:
             list = os.listdir(path)
@@ -31,56 +25,43 @@ class Handler(BaseHTTPRequestHandler):
         while path.startswith("/"):
             path = path.strip("/")
 
-        return path
-
-    def parseDirPath(self, path):
-        if not path.endswith("/"):
-            path = path + "/"
+        if not os.path.exists(path):
+            return None
 
         return path
+
+    def setContentType(self, path):
+        if os.path.isdir(path):
+            return 'text/plain; charset=utf-8'
+        else:
+            return 'application/octet-stream'
 
     def do_GET(self):
         try:
             path = self.parsePath(self.path)
 
             if path == None:
-                self.send_error(403, "forbidden page")
+                self.send_error(404, "file not found")
                 return
 
-            if self.isDir(path):
-                path = self.parseDirPath(path)
+            # Send code 200 response
+            self.send_response(200)
 
+            # Send header first
+            self.send_header('Content-type', self.setContentType(path))
+            self.end_headers()
+
+            # Send body of the message
+            if os.path.isdir(path):
                 index = self.getIndex(path)
 
-                # Send code 200 response
-                self.send_response(200)
-
-                # Send header first
-                self.send_header('Content-type','text/plain; charset=utf-8')
-                self.end_headers()
-
-                # Send index to client
                 for elem in index:
-                    self.wfile.write(URL + path + elem + '\n')
-
-                return
-
-            if self.isFile(path):
-                # Send code 200 response
-                self.send_response(200)
-
-                # Send header first
-                self.send_header('Content-type','application/octet-stream')
-                self.end_headers()
-
-                # Send file to client
+                    self.wfile.write(URL + path + '/' + elem + '\n')
+            else:
                 file = open(path, 'rb')
                 shutil.copyfileobj(file, self.wfile)
                 file.close()
 
-                return
-
-            self.send_error(404, "file not found")
             return
 
         except IOError:
